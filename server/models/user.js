ed09cf6240cf3eac64fc489852d4f6ae55efe2e8
Shivang <jaiswalshivang01@gmail.com>
2025-07-27 09:40:42 +0530

    Fix the user model issue

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,21 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
+  // Extended user schema with role-based access and location info
+  role: {
+    type: String,
+    enum: ['visitor', 'nursery_admin', 'admin'],
+    default: 'visitor'
+  },
+
+  nurseryName: {
+    type: String
+  },
+
+  location: {
+    type: String
+  },
+
   username: {
     type: String,
     required: true,
@@ -19,27 +34,33 @@ const userSchema = new mongoose.Schema({
       ref: "Blog",
     },
   ],
+  token: {
+    type: String,
+  },
+
+  // Articles the user has read
   articlesRead: [
     {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Article",
-    }
+    },
   ],
+
+  // Articles the user has written
   articlesWritten: [
     {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Article",
     }
   ],
+
+  // Plants the user has added/saved
   userPlants: [
     {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Plant",
-    }
+    },
   ],
-  token: {
-    type: String,
-  },
 });
 
 exports.User = mongoose.model("User", userSchema);
